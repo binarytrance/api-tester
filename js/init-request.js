fbc27b0dad9d3a9cb0e976e71afe5d7afdfc06fd
binarytrance <ganeshan@lollypop.biz>
2018-08-07 03:21:55 +0530

    added button functionality

--- js/init-request.js
+++ js/init-request.js
@@ -1,14 +1,76 @@
 (function() {
+	var idVal = 0;
 	var httpRequest;
 	var submitButton = document.getElementById("submit-btn");
 	var requestType;
 	var statusCode;
 	var jsonOutput;
+	var addQueryButton = document.getElementById("addQuery");
+	var queryInput = document.getElementById("queryGroup-1");
+	var headerInput = document.getElementById("headerGroup-1");
+	var addHeaderButton = document.getElementById("addHeader");
+	var deleteButton = document.getElementsByClassName("delete")[0];
+	console.log(deleteButton);
 	submitButton.addEventListener("click", makeRequest);
 
-	$("#addQuery").on("click", function(e) {
-		e.preventDefault();
-	})
+	// $("#addQuery").on("click", function(e) {
+	// 	e.preventDefault();
+	// })
+
+	addQueryButton.addEventListener("click", addNewQuery);
+	addHeaderButton.addEventListener("click", addNewHeader);
+	deleteButton.addEventListener("click", deleteNode);
+
+	function deleteNode() {
+		console.log(90990)
+		console.log(this.parentNode)
+	}
+
+	function addNewQuery() {
+		console.log(document.getElementById("queryGroup-1").length);
+		if(queryInput) {
+			var queryGroupLength = document.getElementsByClassName("query-group").length;
+			console.log(queryGroupLength, document.getElementsByClassName("query-group")[queryGroupLength - 1]);
+			var lastElement = document.getElementsByClassName("query-group")[queryGroupLength - 1].dataset.serial;
+			console.log(lastElement);
+			idVal = lastElement;
+			idVal++;
+		}
+		else {
+			idVal = 1;
+		}
+		console.log(idVal);
+		var queryClone = queryInput.cloneNode(true);
+		queryClone.id = "queryGroup-" + idVal;
+		queryClone.dataset.serial = idVal;
+		console.log(queryClone);
+		document.getElementsByClassName("input-group--query")[0].appendChild(queryClone)
+	}
+
+	function addNewHeader() {
+		console.log(document.getElementById("queryGroup-1").length);
+		if(headerInput) {
+			var headerGroupLength = document.getElementsByClassName("header-group").length;
+			console.log(headerGroupLength, document.getElementsByClassName("header-group")[headerGroupLength - 1]);
+			var lastElement = document.getElementsByClassName("header-group")[headerGroupLength - 1].dataset.serial;
+			console.log(lastElement);
+			idVal = lastElement;
+			idVal++;
+		}
+		else {
+			idVal = 1;
+		}
+		console.log(idVal);
+		var headerClone = headerInput.cloneNode(true);
+		headerClone.id = "headerGroup-" + idVal;
+		headerClone.dataset.serial = idVal;
+		console.log(headerClone);
+		document.getElementsByClassName("input-group--header")[0].appendChild(headerClone)
+	}
+
+	function deleteNode() {
+
+	}
 
 	function makeRequest() {
 
